cb16cc167112f57c0e540dc62c2742b83fd96744
Jens Astrup <jensaiden@gmail.com>
2024-01-16 22:01:39 -0500

    Remove unnecessary comments

--- src/tests/service_worker/getStoryId.test.js
+++ src/tests/service_worker/getStoryId.test.js
@@ -1,11 +1,9 @@
-// Mock the chrome API
 import {getStoryId} from '../../js/service_worker';
 
 
 global.chrome = {
     tabs: {
         query: jest.fn((queryInfo, callback) => {
-            // Mock implementation of chrome.tabs.query
             callback([{ url: 'http://example.com/story/12345' }]);
         }),
     },
@@ -18,11 +16,8 @@ global.chrome = {
 describe('service_worker tests', () => {
     describe('getStoryId', () => {
         it('should return the correct story ID from the URL', async () => {
-            // Call getStoryId and check the result
             const storyId = await getStoryId();
             expect(storyId).toBe('12345');
         });
-
-        // Additional tests...
     });
 });
