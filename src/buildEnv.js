6e546e2e6565de2ebb045411710ff763ad05d180
Jens Astrup <jens@wellthy.com>
2024-01-20 16:41:53 -0500

    Set up environment variables for manifest file
    This commit sets up the infrastructure to use environment variables in the manifest file for the chrome extension. Manifest.json is renamed to manifest.template.json, and a new buildEnv script is introduced to generate the manifest.json file using the environment variables. A dotenv dependency is added to manage these environment variables.

---
+++ src/buildEnv.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+require('dotenv').config();
+
+const manifestTemplate = fs.readFileSync('manifest.template.json', 'utf8');
+const manifest = manifestTemplate
+ .replace(`__CLIENT_ID__`, process.env.CLIENT_ID)
+ .replace(`__CHROME_KEY__`, process.env.CHROME_KEY);
+
+fs.writeFileSync('manifest.json', manifest);
