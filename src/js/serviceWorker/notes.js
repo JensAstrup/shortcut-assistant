35ce3217a0f8999e49c46e1fec1fddc2c5d35fcd
Jens Astrup <jens@wellthy.com>
2024-01-24 08:39:26 -0500

    Refactor service worker code, start using Sentry releases

---
+++ src/js/serviceWorker/notes.js
@@ -0,0 +1,14 @@
+import {getStoryId} from './utils';
+
+
+export function getNotesKey(storyId) {
+    return "notes_" + storyId;
+}
+
+
+export async function getNotes() {
+    const storyId = await getStoryId();
+    const key = getNotesKey(storyId);
+    const result = await chrome.storage.sync.get(key);
+    return result[key];
+}
