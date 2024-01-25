800b9acf0da83fc985eb3cd4d8db8fb71362e7fa
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-01-24 19:27:45 -0500

    Respect settings (#52)

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,16 @@
 import {sleep} from "./utils";
+import {getSyncedSetting} from './serviceWorker/utils';
 
 const saveButton = document.getElementById('saveKeyButton');
 const analyzeButton = document.getElementById('analyzeButton');
+const stalledWorkCheckbox = document.getElementById('stalledWorkToggle');
+const todoistCheckbox = document.getElementById('todoistOptions');
 
 saveButton.addEventListener('click', async function() {
     saveButton.disabled = true
     const openAIToken = document.getElementById('openAIToken').value;
-    const enableStalledWorkWarnings = document.getElementById('stalledWorkToggle').checked;
-    const enableTodoistOptions = document.getElementById('todoistOptions').checked;
+    const enableStalledWorkWarnings = stalledWorkCheckbox.checked;
+    const enableTodoistOptions = todoistCheckbox.checked;
     await chrome.storage.local.set({'openAIToken': openAIToken })
     await chrome.storage.sync.set({'enableStalledWorkWarnings': enableStalledWorkWarnings })
     await chrome.storage.sync.set({'enableTodoistOptions': enableTodoistOptions })
@@ -19,27 +22,31 @@ saveButton.addEventListener('click', async function() {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+function setSectionDisplay(tabToShow, sectionToShow, tabToHide, sectionToHide) {
+    tabToShow.addEventListener('click', function(e) {
+        e.preventDefault();
+        sectionToShow.classList.remove('hidden');
+        sectionToHide.classList.add('hidden');
+        tabToShow.classList.add('-mb-px');
+        tabToHide.classList.remove('-mb-px');
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', async function() {
     const tabActions = document.getElementById('tabActions');
     const tabSettings = document.getElementById('tabSettings');
     const actionsSection = document.getElementById('actionsSection');
     const settingsSection = document.getElementById('settingsSection');
 
-    tabActions.addEventListener('click', function(e) {
-        e.preventDefault();
-        actionsSection.classList.remove('hidden');
-        settingsSection.classList.add('hidden');
-        tabActions.classList.add('-mb-px');
-        tabSettings.classList.remove('-mb-px');
-    });
+    const enableStalledWorkWarnings = await getSyncedSetting('enableStalledWorkWarnings', true)
+    const enableTodoistOptions = await getSyncedSetting('enableTodoistOptions', false)
 
-    tabSettings.addEventListener('click', function(e) {
-        e.preventDefault();
-        settingsSection.classList.remove('hidden');
-        actionsSection.classList.add('hidden');
-        tabSettings.classList.add('-mb-px');
-        tabActions.classList.remove('-mb-px');
-    });
+    stalledWorkCheckbox.checked = enableStalledWorkWarnings
+    todoistCheckbox.checked = enableTodoistOptions
+
+    setSectionDisplay(tabActions, actionsSection, tabSettings, settingsSection);
+    setSectionDisplay(tabSettings, settingsSection, tabActions, actionsSection);
 });
 
 tailwind.config = {
