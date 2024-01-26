e31a8bda519dabfea83476622cd06a30227e2ea5
Jens Astrup <jens@wellthy.com>
2024-01-25 19:29:50 -0500

    Update manifest and build scripts

--- src/buildManifest.js
+++ src/buildManifest.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const manifestTemplate = fs.readFileSync('manifest.template.json', 'utf8');
 const manifest = manifestTemplate
- .replace(`__CLIENT_ID__`, process.env.CLIENT_ID)
  .replace(`__VERSION__`, process.env.VERSION)
- .replace(`__CHROME_KEY__`, process.env.CHROME_KEY);
 
 fs.writeFileSync('manifest.json', manifest);
