8eac135936fa901f035ba28b8f66e5c2a3b84f91
Jens Astrup <jensaiden@gmail.com>
2024-02-13 09:00:02 -0500

    Formatting

--- src/js/serviceWorker/omnibox/redirectFromOmnibox.test.js
+++ src/js/serviceWorker/omnibox/redirectFromOmnibox.test.js
@@ -2,60 +2,63 @@
  * @jest-environment jsdom
  */
 
-import { redirectFromOmnibox } from './omnibox';
+import {redirectFromOmnibox} from './omnibox'
 
 // Global mock for chrome.tabs
 global.chrome = {
-  tabs: {
-    update: jest.fn(),
-    create: jest.fn()
-  }
-};
+    tabs: {
+        update: jest.fn(),
+        create: jest.fn()
+    }
+}
 
 describe('redirectFromOmnibox', () => {
 
-  afterEach(() => {
-    global.chrome.tabs.update.mockReset();
-    global.chrome.tabs.create.mockReset();
-  });
+    afterEach(() => {
+        global.chrome.tabs.update.mockReset()
+        global.chrome.tabs.create.mockReset()
+    })
 
-  it('should update the current tab with the correct URL when the disposition is currentTab', () => {
-    const text = 'test';
-    const disposition = 'currentTab';
+    it('should update the current tab with the correct URL when the disposition is currentTab', () => {
+        const text = 'test'
+        const disposition = 'currentTab'
 
-    const expectedUrl = { url: `https://app.shortcut.com/search#${encodeURIComponent(text)}` };
+        const expectedUrl = {url: `https://app.shortcut.com/search#${encodeURIComponent(text)}`}
 
-    redirectFromOmnibox(text, disposition);
-    expect(global.chrome.tabs.update).toHaveBeenCalledWith(expectedUrl);
-  });
+        redirectFromOmnibox(text, disposition)
+        expect(global.chrome.tabs.update).toHaveBeenCalledWith(expectedUrl)
+    })
 
-  it('should create a new foreground tab with the correct URL when the disposition is newForegroundTab', () => {
-    const text = 'test';
-    const disposition = 'newForegroundTab';
+    it('should create a new foreground tab with the correct URL when the disposition is newForegroundTab', () => {
+        const text = 'test'
+        const disposition = 'newForegroundTab'
 
-    const expectedUrl = { url: `https://app.shortcut.com/search#${encodeURIComponent(text)}` };
+        const expectedUrl = {url: `https://app.shortcut.com/search#${encodeURIComponent(text)}`}
 
-    redirectFromOmnibox(text, disposition);
-    expect(global.chrome.tabs.create).toHaveBeenCalledWith(expectedUrl);
-  });
+        redirectFromOmnibox(text, disposition)
+        expect(global.chrome.tabs.create).toHaveBeenCalledWith(expectedUrl)
+    })
 
-  it('should create a new background tab with the correct URL when the disposition is newBackgroundTab', () => {
-    const text = 'test';
-    const disposition = 'newBackgroundTab';
+    it('should create a new background tab with the correct URL when the disposition is newBackgroundTab', () => {
+        const text = 'test'
+        const disposition = 'newBackgroundTab'
 
-    const expectedUrl = { url: `https://app.shortcut.com/search#${encodeURIComponent(text)}`, active: false };
+        const expectedUrl = {
+            url: `https://app.shortcut.com/search#${encodeURIComponent(text)}`,
+            active: false
+        }
 
-    redirectFromOmnibox(text, disposition);
-    expect(global.chrome.tabs.create).toHaveBeenCalledWith(expectedUrl);
-  });
+        redirectFromOmnibox(text, disposition)
+        expect(global.chrome.tabs.create).toHaveBeenCalledWith(expectedUrl)
+    })
 
-  it('should update the tab with the correct url when the disposition is unknown', () => {
-    const text = 'test';
-    const disposition = 'unknown';
+    it('should update the tab with the correct url when the disposition is unknown', () => {
+        const text = 'test'
+        const disposition = 'unknown'
 
-    const expectedUrl = { url: `https://app.shortcut.com/search#${encodeURIComponent(text)}` };
+        const expectedUrl = {url: `https://app.shortcut.com/search#${encodeURIComponent(text)}`}
 
-    redirectFromOmnibox(text, disposition);
-    expect(global.chrome.tabs.update).toHaveBeenCalledWith(expectedUrl);
-  });
-});
+        redirectFromOmnibox(text, disposition)
+        expect(global.chrome.tabs.update).toHaveBeenCalledWith(expectedUrl)
+    })
+})
