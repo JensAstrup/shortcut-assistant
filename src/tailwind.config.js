59299ba188ed5a77d321bcf616d3d763ebbf1f79
Jens Astrup <jensaiden@gmail.com>
2024-02-16 17:51:13 -0500

    Complete styling and update typography

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,7 +4,7 @@ content: ['./html/**', './js/popup/**'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
