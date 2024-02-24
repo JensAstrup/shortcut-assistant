26fa86e49164fb65b1f38d2ff09bba37ecac1a02
Jens Astrup <jensaiden@gmail.com>
2024-02-23 21:11:46 -0500

    Add test for sendEvent errors

--- src/js/analyze/AiFunctions.test.js
+++ src/js/analyze/AiFunctions.test.js
@@ -6,29 +6,30 @@ import * as utilsModule from '../utils/utils'
 import {AiFunctions} from './aiFunctions'
 import * as Sentry from '@sentry/browser'
 
+
 jest.mock('../analytics/event', () => ({
-    sendEvent: jest.fn().mockResolvedValue(undefined)
+  sendEvent: jest.fn().mockResolvedValue(undefined)
 }))
 
 jest.mock('../utils/utils', () => ({
-    sleep: jest.fn().mockResolvedValue(undefined)
+  sleep: jest.fn().mockResolvedValue(undefined)
 }))
 
 jest.mock('@sentry/browser', () => ({
-    captureException: jest.fn()
+  captureException: jest.fn()
 }))
 
 global.chrome = {
-    tabs: {
-        query: jest.fn().mockImplementation((queryInfo, callback) => {
-            callback([{id: 1}])
-        }),
-        sendMessage: jest.fn()
-    }
+  tabs: {
+    query: jest.fn().mockImplementation((queryInfo, callback) => {
+      callback([{id: 1}])
+    }),
+    sendMessage: jest.fn()
+  }
 }
 
-function setupDOM(){
-    document.body.innerHTML = `
+function setupDOM() {
+  document.body.innerHTML = `
     <div id="analyzeButton">
     <span id="loadingSpan"></span>
     </div>
@@ -38,52 +39,63 @@ function setupDOM(){
 }
 
 describe('OpenAI class', () => {
-    beforeEach(() => {
-        setupDOM()
-        jest.clearAllMocks()
+  beforeEach(() => {
+    setupDOM()
+    jest.clearAllMocks()
+  })
+
+  describe('analyzeStoryDetails', () => {
+    it('should append loading spinner and send message to active tab', async () => {
+      await AiFunctions.analyzeStoryDetails()
+
+      const loadingSpan = document.getElementById('loadingSpan')
+      expect(loadingSpan).not.toBeNull()
+      expect(chrome.tabs.query).toHaveBeenCalled()
+      expect(chrome.tabs.sendMessage).toHaveBeenCalledWith(1, {message: 'analyzeStoryDescription'})
+      expect(sendEvent).toHaveBeenCalledWith('analyze_story_details')
+    })
+    it('should catch sendEvent error and capture exception', async () => {
+      const error = new Error('error')
+      sendEvent.mockRejectedValue(error)
+      console.error = jest.fn()
+      await AiFunctions.analyzeStoryDetails()
+
+      expect(sendEvent).toHaveBeenCalledWith('analyze_story_details')
+      expect(console.error).toHaveBeenCalledWith(error)
+      expect(Sentry.captureException).toHaveBeenCalledWith(error)
+    })
+  })
+
+  describe('processOpenAIResponse', () => {
+    it('should handle OpenAIResponseCompleted message', async () => {
+      await AiFunctions.processOpenAIResponse({type: 'OpenAIResponseCompleted'})
+
+      const analyzeText = document.getElementById('analyzeText')
+      expect(analyzeText.textContent).toBe('Analyze Story')
+      expect(document.getElementById('loadingSpan')).toBeNull()
     })
 
-    describe('analyzeStoryDetails', () => {
-        it('should append loading spinner and send message to active tab', async () => {
-            await AiFunctions.analyzeStoryDetails()
+    it('should handle OpenAIResponseFailed message and hide errorState after 6 seconds', async () => {
+      jest.useFakeTimers()
+      await AiFunctions.processOpenAIResponse({type: 'OpenAIResponseFailed'})
 
-            const loadingSpan = document.getElementById('loadingSpan')
-            expect(loadingSpan).not.toBeNull()
-            expect(chrome.tabs.query).toHaveBeenCalled()
-            expect(chrome.tabs.sendMessage).toHaveBeenCalledWith(1, {message: 'analyzeStoryDescription'})
-            expect(sendEvent).toHaveBeenCalledWith('analyze_story_details')
-        })
-        it('should catch sendEvent error and capture exception', async () => {
-            const error = new Error('error')
-            sendEvent.mockRejectedValue(error)
-            console.error = jest.fn()
-            await AiFunctions.analyzeStoryDetails()
+      const errorState = document.getElementById('errorState')
+      expect(utilsModule.sleep).toHaveBeenCalledWith(6000)
+      jest.advanceTimersByTime(6000)
 
-            expect(sendEvent).toHaveBeenCalledWith('analyze_story_details')
-            expect(console.error).toHaveBeenCalledWith(error)
-            expect(Sentry.captureException).toHaveBeenCalledWith(error)
-        })
+      expect(errorState.style.display).toBe('none')
+      expect(sendEvent).toHaveBeenCalledWith('analyze_story_details_failed')
     })
 
-    describe('processOpenAIResponse', () => {
-        it('should handle OpenAIResponseCompleted message', async () => {
-            await AiFunctions.processOpenAIResponse({type: 'OpenAIResponseCompleted'})
+    it('should catch sendEvent error and capture exception', async () => {
+      const error = new Error('error')
+      sendEvent.mockRejectedValue(error)
+      console.error = jest.fn()
+      await AiFunctions.processOpenAIResponse({type: 'OpenAIResponseFailed'})
 
-            const analyzeText = document.getElementById('analyzeText')
-            expect(analyzeText.textContent).toBe('Analyze Story')
-            expect(document.getElementById('loadingSpan')).toBeNull()
-        })
-
-        it('should handle OpenAIResponseFailed message and hide errorState after 6 seconds', async () => {
-            jest.useFakeTimers()
-            await AiFunctions.processOpenAIResponse({type: 'OpenAIResponseFailed'})
-
-            const errorState = document.getElementById('errorState')
-            expect(utilsModule.sleep).toHaveBeenCalledWith(6000)
-            jest.advanceTimersByTime(6000)
-
-            expect(errorState.style.display).toBe('none')
-            expect(sendEvent).toHaveBeenCalledWith('analyze_story_details_failed')
-        })
+      expect(sendEvent).toHaveBeenCalledWith('analyze_story_details_failed')
+      expect(console.error).toHaveBeenCalledWith(error)
+      expect(Sentry.captureException).toHaveBeenCalledWith(error)
     })
+  })
 })
