d0f0a1de27c454a63e397cdf7db3977b949be59c
Jens Astrup <jensaiden@gmail.com>
2024-02-26 22:34:30 -0500

    Refactor installAndUpdate into class and conditionally set indicator, update tests
    Conditionally display new version indicator depending on version changelog was updated for. Also refactored install/update code into a class and added full test coverage

--- src/js/serviceWorker/onInstallAndUpdate.js
+++ src/js/serviceWorker/onInstallAndUpdate.js
@@ -1,24 +1,34 @@
-export function onInstall(){
-    chrome.windows.create({
-        url: '../html/installed.html',
-        type: 'popup',
-        width: 310,
-        height: 500
-    })
-    chrome.storage.sync.set({'enableStalledWorkWarnings': true})
-    chrome.storage.sync.set({'enableTodoistOptions': false})
-}
+import * as Sentry from '@sentry/browser'
 
-export async function onUpdate(){
-    await chrome.action.setBadgeText({text: ' '})
-    await chrome.action.setBadgeBackgroundColor({color: '#a30000'})
+
+export class InstallAndUpdate {
+    static onInstall() {
+        chrome.windows.create({
+            url: '../html/installed.html',
+            type: 'popup',
+            width: 310,
+            height: 500
+        })
+        chrome.storage.sync.set({'enableStalledWorkWarnings': true})
+        chrome.storage.sync.set({'enableTodoistOptions': false})
+    }
+
+    static async onUpdate() {
+        await chrome.action.setBadgeText({text: ' '})
+        await chrome.action.setBadgeBackgroundColor({color: '#a30000'})
+    }
 }
 
 export function onInstallAndUpdate(details){
     if (details.reason === 'install') {
-        onInstall(details)
+        InstallAndUpdate.onInstall(details)
     }
     else if (details.reason === 'update') {
-        onUpdate(details)
+        if (process.env.CHANGELOG_VERSION === process.env.VERSION) {
+            InstallAndUpdate.onUpdate(details).catch(e => {
+                console.error('Error updating:', e)
+                Sentry.captureException(e)
+            })
+        }
     }
 }
--- src/js/serviceWorker/tests/oninstallAndUpdate.test.js
+++ src/js/serviceWorker/tests/oninstallAndUpdate.test.js
@@ -1,4 +1,4 @@
-import {onInstall, onUpdate} from '../onInstallAndUpdate'
+import {InstallAndUpdate, onInstallAndUpdate} from '../onInstallAndUpdate'
 
 describe('onInstall function', () => {
     beforeEach(() => {
@@ -8,7 +8,7 @@ describe('onInstall function', () => {
     })
 
     test('it sets initial configuration and opens installed.html', () => {
-        onInstall()
+      InstallAndUpdate.onInstall()
 
         expect(chrome.windows.create).toHaveBeenCalledTimes(1)
         expect(chrome.windows.create).toHaveBeenCalledWith({
@@ -24,14 +24,41 @@ describe('onInstall function', () => {
     })
 })
 
-describe('onUpdate function', () => {
+describe('onUpdate function ', () => {
     beforeEach(() => {
         chrome.windows.create.mockClear()
     })
-    test('it opens updated.html', async () => {
-        await onUpdate()
+  test('it sets badge text and color', async () => {
+    await InstallAndUpdate.onUpdate()
 
         expect(chrome.action.setBadgeText).toHaveBeenCalledWith({text: ' '})
         expect(chrome.action.setBadgeBackgroundColor).toHaveBeenCalledWith({color: '#a30000'})
     })
+})
+
+describe('onInstallAndUpdate function', () => {
+  let onInstall, onUpdate
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    onInstall = jest.spyOn(InstallAndUpdate, 'onInstall').mockResolvedValue(null)
+    onUpdate = jest.spyOn(InstallAndUpdate, 'onUpdate').mockResolvedValue(null)
+  })
+  test('it calls onInstall when reason is install', () => {
+    onInstallAndUpdate({reason: 'install'})
+
+    expect(onInstall).toHaveBeenCalledTimes(1)
+  })
+  test('it calls onUpdate when reason is update and there is an updated changelog', () => {
+    process.env.CHANGELOG_VERSION = '1.0.0'
+    process.env.VERSION = '1.0.0'
+    onInstallAndUpdate({reason: 'update'})
+    expect(onUpdate).toHaveBeenCalledTimes(1)
+  })
+  test('it does not call onUpdate when reason is update and there is no updated changelog', () => {
+    process.env.CHANGELOG_VERSION = '1.0.1'
+    process.env.VERSION = '1.0.0'
+    onInstallAndUpdate({reason: 'update'})
+    expect(onUpdate).not.toHaveBeenCalled()
+  })
 })
