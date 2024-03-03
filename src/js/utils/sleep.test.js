e56a7642a44317290412c00456b2eb07ff261c0f
Jens Astrup <jensaiden@gmail.com>
2024-03-02 19:20:07 -0500

    Refactor utility functions and related tests .

---
+++ src/js/utils/sleep.test.js
@@ -0,0 +1,23 @@
+import {sleep} from './sleep' // Adjust the import path according to your project structure
+
+describe('sleep function', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  afterEach(() => {
+    jest.useRealTimers()
+  })
+
+  it('should complete after the specified time using fake timers', async () => {
+    setTimeout = jest.fn(setTimeout)
+    const promise = sleep(1000)
+
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
+
+    jest.advanceTimersByTime(1000)
+
+    await promise
+  })
+})
---
+++ src/js/utils/storyPageIsReady.js
@@ -0,0 +1,14 @@
+import {sleep} from './sleep'
+
+
+export async function storyPageIsReady() {
+  let storyTitle = document.querySelector('.story-name')
+  let loop = 0
+  while (storyTitle === null) {
+    await sleep(loop * 1000)
+    storyTitle = document.querySelector('.story-name')
+    loop += 1.5
+  }
+  await sleep(500)
+  return true
+}
