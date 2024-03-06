5a9f8e54dd3ccda86c3875ae11eb3c73c7a36da1
Jens Astrup <jensaiden@gmail.com>
2024-03-06 12:53:05 -0500

    Remove moment from codebase, add dayjs

--- src/js/utils/hoursBetweenExcludingWeekends.js
+++ src/js/utils/hoursBetweenExcludingWeekends.js
@@ -1,26 +1,26 @@
-import moment from 'moment/moment'
+import dayjs from 'dayjs'
 
 
-export function hoursBetweenExcludingWeekends(startDateStr, endDateStr){
-    const shortcutDateFormat = 'MMM D YYYY, h:mm: a'
-    const startDate = moment(startDateStr, shortcutDateFormat)
-    let endDate = endDateStr ? moment(endDateStr, shortcutDateFormat) : moment()
+export function hoursBetweenExcludingWeekends(startDateStr, endDateStr) {
+    const shortcutDateFormat = 'MMM D YYYY, h:mm A' // Ensure this matches Day.js formatting tokens
+    const startDate = dayjs(startDateStr, shortcutDateFormat)
+    let endDate = endDateStr ? dayjs(endDateStr, shortcutDateFormat) : dayjs()
 
     let hours = 0
-    let currentDate = moment(startDate)
-    while (currentDate < endDate) {
+    let currentDate = startDate.clone()
+    while (currentDate.isBefore(endDate)) {
         if (currentDate.day() !== 0 && currentDate.day() !== 6) { // 0 is Sunday, 6 is Saturday
             hours += 24
         }
-        currentDate.add(1, 'days')
+        currentDate = currentDate.add(1, 'day')
     }
 
     if (startDate.day() !== 0 && startDate.day() !== 6) {
-        hours -= startDate.hours() + startDate.minutes() / 60
+        hours -= startDate.hour() + startDate.minute() / 60
     }
 
     if (endDate.day() !== 0 && endDate.day() !== 6) {
-        hours += endDate.hours() + endDate.minutes() / 60
+        hours += endDate.hour() + endDate.minute() / 60
     }
 
     return hours
