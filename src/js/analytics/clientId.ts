85c62244b83ad7dce5eb30d44b6350861e400687
Jens Astrup <jensaiden@gmail.com>
2024-03-06 10:12:14 -0500

    Attempt using uuid package to support CI tests

--- src/js/analytics/clientId.ts
+++ src/js/analytics/clientId.ts
@@ -1,8 +1,10 @@
+import {v4 as uuidv4} from 'uuid';
+
 export async function getOrCreateClientId(): Promise<string> {
     const result = await chrome.storage.sync.get('clientId');
-    let clientId = result.clientId;
+    let clientId: string = result.clientId;
     if (!clientId) {
-        clientId = crypto.randomUUID();
+        clientId = uuidv4();
         await chrome.storage.sync.set({clientId});
     }
     return clientId;
