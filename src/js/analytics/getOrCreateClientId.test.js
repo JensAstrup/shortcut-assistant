9ff814cc724f8f2f6911204d3119bb1617128cba
Jens Astrup <jensaiden@gmail.com>
2024-03-06 08:59:09 -0500

    Do not use typescript for testing

--- src/js/analytics/getOrCreateClientId.test.js
+++ src/js/analytics/getOrCreateClientId.test.js
@@ -25,14 +25,14 @@ describe('getOrCreateClientId function', () => {
 
     it('should return clientId when there is an existing Id in chrome storage', async () => {
         const expectedClientId = 'randomClientId';
-        get.mockResolvedValue({clientId: expectedClientId} as never);
+        get.mockResolvedValue({clientId: expectedClientId})
         const result = await getOrCreateClientId();
         expect(get).toHaveBeenCalledWith('clientId');
         expect(result).toBe(expectedClientId);
     });
 
     it('should create and return a new clientId when there isn\'t an existing Id in chrome storage', async () => {
-        get.mockResolvedValue({} as never);
+        get.mockResolvedValue({})
         const result = await getOrCreateClientId();
         expect(get).toHaveBeenCalledWith('clientId');
         expect(set).toHaveBeenCalledWith({clientId: result});
