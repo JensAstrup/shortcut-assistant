dede99e2e3b91c827c8e95aba382b90e610bed05
Jens Astrup <jensaiden@gmail.com>
2024-03-06 09:20:43 -0500

    Remove console log and lingering jest-chrome setup

--- src/js/analytics/clientId.ts
+++ src/js/analytics/clientId.ts
@@ -1,6 +1,5 @@
 export async function getOrCreateClientId(): Promise<string> {
     const result = await chrome.storage.sync.get('clientId');
-    console.log(result);
     let clientId = result.clientId;
     if (!clientId) {
         clientId = crypto.randomUUID();
