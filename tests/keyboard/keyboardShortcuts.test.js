2fa91bf835443dc6eb74ea5aa963817f629243c1
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-04-03 08:37:04 -0400

    SHA-119: Convert keyboard to typescript (#262)

--- tests/keyboard/keyboardShortcuts.test.js
+++ tests/keyboard/keyboardShortcuts.test.js
@@ -47,6 +47,22 @@ describe('Shortcuts', () => {
     expect(document.getElementById).toHaveBeenCalledWith('story-dialog-state-dropdown')
   })
 
+  it('should log error to console if no popup is found in changeState function', async () => {
+    console.error = jest.fn()
+    const mockedDropdown = {click: jest.fn()}
+    const mockedPopup = {querySelector: jest.fn().mockReturnValue(null)}
+
+
+    document.getElementById.mockReturnValueOnce(mockedDropdown)
+    document.querySelector.mockReturnValueOnce(null)
+
+    instance = new KeyboardShortcuts()
+    await instance.changeState()
+
+    expect(mockedDropdown.click).toHaveBeenCalled()
+    expect(mockedPopup.querySelector).not.toHaveBeenCalled()
+  })
+
   test('changeState function with no input', async () => {
     const mockedDropdown = {click: jest.fn()}
     const mockedPopup = {querySelector: jest.fn()}
@@ -90,6 +106,49 @@ describe('Shortcuts', () => {
     expect(mockedIterationSelect.click).toHaveBeenCalled()
     expect(mockedIterationPopup.querySelector).toHaveBeenCalledWith('.autocomplete-input')
     expect(mockInput.focus).toHaveBeenCalled()
+  })
+
+  it('should error to console if no input select is found in changeIteration function', async () => {
+    const mockChildButton = {click: jest.fn()}
+    console.log = jest.fn()
+    document.querySelector.mockReturnValueOnce(null)
+    const mockedIterationSelect = {
+      querySelector: jest.fn().mockReturnValue(mockChildButton),
+      click: jest.fn()
+    }
+
+    await instance.changeIteration()
+
+    expect(mockedIterationSelect.querySelector).not.toHaveBeenCalledWith('[role="button"]')
+    expect(mockChildButton.click).not.toHaveBeenCalled()
+    expect(mockedIterationSelect.click).not.toHaveBeenCalled()
+  })
+
+  it('should error to console if no input is found in changeIteration function', async () => {
+    const mockChildButton = {click: jest.fn()}
+    const mockedIterationSelect = {
+      querySelector: jest.fn().mockReturnValue(mockChildButton),
+      click: jest.fn()
+    }
+    const mockInput = {focus: jest.fn(), value: ''}
+    const mockedIterationPopup = {querySelector: jest.fn().mockReturnValue(null)}
+
+    document.querySelector.mockImplementation((selector) => {
+      if (selector === '[data-perma-id="iteration-select"]') {
+        return mockedIterationSelect
+      }
+      else if (selector === '.iteration-selector') {
+        return mockedIterationPopup
+      }
+    })
+
+    await instance.changeIteration()
+
+    expect(mockedIterationSelect.querySelector).toHaveBeenCalledWith('[role="button"]')
+    expect(mockChildButton.click).toHaveBeenCalled()
+    expect(mockedIterationSelect.click).toHaveBeenCalled()
+    expect(mockedIterationPopup.querySelector).toHaveBeenCalledWith('.autocomplete-input')
+    expect(mockInput.focus).not.toHaveBeenCalled()
   })
 
   test('changeIteration function with no childButton', async () => {
@@ -157,6 +216,35 @@ describe('Shortcuts', () => {
 
     expect(mockedGitHelpers.click).toHaveBeenCalled()
     expect(global.navigator.clipboard.writeText).toHaveBeenCalled()
+  })
+
+  it('should error to console when no githelpers are found in copyGitBranch function', async () => {
+    const mockedGitHelpers = {click: jest.fn()}
+    console.error = jest.fn()
+    document.getElementById.mockReturnValueOnce(null)
+
+    global.navigator.clipboard = {writeText: jest.fn()}
+
+    await instance.copyGitBranch()
+
+    expect(console.error).toHaveBeenCalledWith('The git helpers dropdown was not found.')
+    expect(mockedGitHelpers.click).not.toHaveBeenCalled()
+    expect(global.navigator.clipboard.writeText).not.toHaveBeenCalled()
+  })
+
+  it('should error to console if git branch input not found in copyGitBranch function', async () => {
+    const mockedGitHelpers = {click: jest.fn()}
+    console.error = jest.fn()
+    document.getElementById.mockReturnValueOnce(mockedGitHelpers)
+    document.querySelector.mockReturnValueOnce(null)
+
+    global.navigator.clipboard = {writeText: jest.fn()}
+
+    await instance.copyGitBranch()
+
+    expect(console.error).toHaveBeenCalledWith('The git branch input was not found.')
+    expect(mockedGitHelpers.click).toHaveBeenCalled()
+    expect(global.navigator.clipboard.writeText).not.toHaveBeenCalled()
   })
 
   test('_getStateDivWithText function', () => {
@@ -228,5 +316,104 @@ describe('Shortcuts', () => {
 
     expect(mockCopyGitBranch).toHaveBeenCalled()
     expect(instance._getStateDivWithText).toHaveBeenCalledWith('In Development')
+  })
+
+  it('should add keydown event listener to document', () => {
+    const addEventListenerSpy = jest.spyOn(document, 'addEventListener')
+    const keyboardShortcuts = new KeyboardShortcuts()
+
+    keyboardShortcuts.activate()
+
+    expect(addEventListenerSpy).toHaveBeenCalledWith('keydown', expect.any(Function))
+
+    addEventListenerSpy.mockRestore()
+  })
+
+  it.each([
+    // Define test cases as [input, expectedOutput]
+    [{key: 'A', shiftKey: false}, 'a-0-0-0-0'],
+    [{key: 'B', shiftKey: true, metaKey: true, altKey: true, ctrlKey: true}, 'b-1-1-1-1'],
+    [{key: 'C', shiftKey: true, ctrlKey: true}, 'c-0-1-0-1'],
+    [{key: 'd', metaKey: true, shiftKey: false}, 'd-1-0-0-0'],
+    [{key: 'e', altKey: true, shiftKey: true}, 'e-0-1-1-0']
+  ])('serializes shortcut %o to "%s"', (input, expectedOutput) => {
+    const keyboardShortcuts = new KeyboardShortcuts()
+    const serialized = keyboardShortcuts.serializeShortcut(input)
+    expect(serialized).toBe(expectedOutput)
+  })
+
+  it('adds a shortcut to the shortcuts map', () => {
+    const keyboardShortcuts = new KeyboardShortcuts()
+    const mockFunction = jest.fn()
+
+    const shortcut = {
+      key: 'n',
+      shiftKey: true,
+      ctrlKey: true,
+      func: mockFunction
+    }
+
+    const expectedSerializedKey = 'n-0-1-0-1'
+
+    keyboardShortcuts.registerShortcut(shortcut)
+
+    expect(keyboardShortcuts.shortcuts.has(expectedSerializedKey)).toBeTruthy()
+
+    // Further, verify that the function mapped to the serialized key is indeed the mockFunction,
+    // considering it's bound to the KeyboardShortcuts instance, we directly compare the reference here
+    const registeredFunction = keyboardShortcuts.shortcuts.get(expectedSerializedKey)
+    expect(registeredFunction).toBeDefined()
+
+    // Since we cannot directly compare bound functions, we'll invoke it and check if the mock was called
+    registeredFunction()
+    expect(mockFunction).toHaveBeenCalled()
+  })
+
+  describe('KeyboardShortcuts class', () => {
+    let keyboardShortcuts
+    let mockFunction
+
+    beforeEach(() => {
+      keyboardShortcuts = new KeyboardShortcuts()
+      mockFunction = jest.fn().mockResolvedValue(undefined) // Simulate a function that returns a promise
+    })
+
+    it('calls the registered function for a matching keydown event', async () => {
+      const shortcut = {
+        key: 'n',
+        shiftKey: true,
+        ctrlKey: true,
+        func: mockFunction
+      }
+
+      keyboardShortcuts.registerShortcut(shortcut)
+      const event = new KeyboardEvent('keydown', {
+        key: 'n',
+        shiftKey: true,
+        ctrlKey: true
+      })
+
+      const preventDefaultSpy = jest.spyOn(event, 'preventDefault')
+      await keyboardShortcuts.handleKeyDown(event)
+
+      expect(preventDefaultSpy).toHaveBeenCalled()
+      expect(mockFunction).toHaveBeenCalled()
+    })
+
+    it('does not call preventDefault or a function for a non-matching keydown event', async () => {
+      const event = new KeyboardEvent('keydown', {
+        key: 'm',
+        shiftKey: false,
+        ctrlKey: false
+      })
+
+      const preventDefaultSpy = jest.spyOn(event, 'preventDefault')
+
+      await keyboardShortcuts.handleKeyDown(event)
+
+      expect(preventDefaultSpy).not.toHaveBeenCalled()
+      // Since no function is registered for this key, the mock function should not be called
+      expect(mockFunction).not.toHaveBeenCalled()
+    })
   })
 })
