31b47a3fa78ff6ff849d5c6a7a341fdb60285f30
Jens Astrup <jensaiden@gmail.com>
2024-04-06 22:36:13 -0400

    Refactor tests to match directory name, linter changes

--- src/js/keyboard-shortcuts/copy-branch-move-to-in-development.ts
+++ src/js/keyboard-shortcuts/copy-branch-move-to-in-development.ts
@@ -3,6 +3,7 @@ import {getStateDiv} from '../utils/get-state-div'
 import changeState from './change-state'
 import copyGitBranch from './copy-git-branch'
 
+
 async function copyBranchAndMoveToInDevelopment(): Promise<void> {
   await copyGitBranch()
   changeState().then(() => {
