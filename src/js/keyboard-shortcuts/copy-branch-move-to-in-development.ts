a8b3930885f65f725440f17002ff62a5bccd8323
Jens Astrup <jensaiden@gmail.com>
2024-04-09 13:31:49 -0400

    Fix broken shortcut after DOM changes

--- src/js/keyboard-shortcuts/copy-branch-move-to-in-development.ts
+++ src/js/keyboard-shortcuts/copy-branch-move-to-in-development.ts
@@ -1,4 +1,5 @@
 import {getStateDiv} from '../utils/get-state-div'
+import sleep from '../utils/sleep'
 
 import changeState from './change-state'
 import copyGitBranch from './copy-git-branch'
@@ -6,7 +7,8 @@ import copyGitBranch from './copy-git-branch'
 
 async function copyBranchAndMoveToInDevelopment(): Promise<void> {
   await copyGitBranch()
-  changeState().then(() => {
+  changeState().then(async () => {
+    await sleep(1000)
     const stateDiv = getStateDiv('In Development')
     if (stateDiv) {
       stateDiv.click()
