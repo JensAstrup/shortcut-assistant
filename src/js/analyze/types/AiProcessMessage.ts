24c0167d3cd985f34423fbd02dec542dce82a74d
Jens Astrup <jensaiden@gmail.com>
2024-05-11 17:27:02 -0400

    Add multiple AI feature buttons, utilize new streaming proxy response

---
+++ src/js/analyze/types/AiProcessMessage.ts
@@ -0,0 +1,14 @@
+import {AiPromptType} from '@sx/analyze/types/ai-prompt-type'
+
+
+export enum AiProcessMessageType {
+  'completed' = 2,
+  'failed' = 1,
+  'updated' = 0,
+}
+
+export interface AiProcessMessage {
+  type: AiProcessMessageType
+  message?: string
+  data?: { content: string, type: AiPromptType }
+}
