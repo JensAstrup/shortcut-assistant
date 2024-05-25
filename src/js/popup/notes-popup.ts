f2d6a76e02ed32b06b54c3cc22f58f3a8f2849bd
Jens Astrup <jensaiden@gmail.com>
2024-05-24 22:32:51 -0400

    Linting changes

--- src/js/popup/notes-popup.ts
+++ src/js/popup/notes-popup.ts
@@ -15,15 +15,17 @@ export class NotesPopup {
     this.saveButton = saveButton
   }
 
-  _autoExpandTextarea(event: Event): void {
+  public static _autoExpandTextarea(event: Event): void {
     const target = event.target as HTMLTextAreaElement
     target.style.height = 'auto'
     target.style.height = (target.scrollHeight) + 'px'
   }
 
-  async resizeInput(): Promise<void> {
+  resizeInput(): void {
     const storyNotesInput = this.getInput()
-    storyNotesInput.addEventListener('input', this._autoExpandTextarea)
+    // Static method doesn't need to be bound
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    storyNotesInput?.addEventListener('input', NotesPopup._autoExpandTextarea)
   }
 
   async save(): Promise<void> {
@@ -31,7 +33,7 @@ export class NotesPopup {
     if (!storyId) {
       throw new Error('Story ID not found')
     }
-    const data = { [this.getKey(storyId)]: this.getInput().value }
+    const data = { [this.getKey(storyId)]: this.getInput()?.value }
     await chrome.storage.sync.set(data)
     this.saveButton.textContent = 'Saved!'
     const TWO_SECONDS = 2000
@@ -39,16 +41,16 @@ export class NotesPopup {
     this.saveButton.textContent = 'Save'
   }
 
-  getInput() {
-    return document.getElementById('storyNotes') as HTMLInputElement
+  getInput(): HTMLInputElement | null {
+    return document.getElementById('storyNotes') as HTMLInputElement | null
   }
 
-  getKey(storyId: string) {
+  getKey(storyId: string): string {
     return 'notes_' + storyId
   }
 
-  async set() {
-    this.resizeInput().catch(console.error)
+  async set(): Promise<void> {
+    this.resizeInput()
     const storyId = await Story.id()
     if (!storyId) {
       throw new Error('Story ID not found')
@@ -56,7 +58,7 @@ export class NotesPopup {
     const key = this.getKey(storyId)
     const storyNotesInput: HTMLInputElement | null = this.getInput()
     const result = await chrome.storage.sync.get(key)
-    const value = result[key]
+    const value = result[key] as string | undefined
     if (storyNotesInput === null) {
       throw new Error('storyNotesInput not found')
     }
@@ -65,7 +67,7 @@ export class NotesPopup {
     }
   }
 
-  static async handleMessage(message: Record<string, string>) {
+  static handleMessage(message: Record<string, string>): void {
     if (message.message === 'checkNotes') {
       new NotesPopup()
     }
--- tests/popup/notes-popup.test.ts
+++ tests/popup/notes-popup.test.ts
@@ -1,5 +1,5 @@
-import {NotesPopup} from '@sx/popup/notes-popup'
-import {Story} from '@sx/utils/story'
+import { NotesPopup } from '@sx/popup/notes-popup'
+import { Story } from '@sx/utils/story'
 
 
 jest.mock('@sx/utils/story', () => ({
@@ -12,7 +12,7 @@ const mockedStory = Story as jest.Mocked<typeof Story>
 jest.mock('@sx/utils/sleep', () => jest.fn(() => Promise.resolve()))
 
 
-const mockElement = (options = {}) => {
+const mockElement = (options = {}): object => {
   return {
     addEventListener: jest.fn(),
     click: jest.fn(),
@@ -36,9 +36,9 @@ describe('NotesPopup', () => {
       <button id="saveButton">Save</button>
     `
     jest.clearAllMocks()
-    global.chrome.storage.sync.set = jest.fn(() => Promise.resolve())
-    global.chrome.storage.sync.get = jest.fn(() => Promise.resolve({}))
-    global.chrome.runtime.onMessage.addListener = jest.fn()
+    chrome.storage.sync.set = jest.fn(() => Promise.resolve())
+    chrome.storage.sync.get = jest.fn(() => Promise.resolve({}))
+    chrome.runtime.onMessage.addListener = jest.fn()
     inputElement = <HTMLInputElement>document.getElementById('storyNotes')
   })
 
@@ -61,14 +61,17 @@ describe('NotesPopup', () => {
     popup.saveButton = {
       // @ts-expect-error - TS doesn't know about disabled
       disabled: false,
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       get textContent() {
         // @ts-expect-error - TS doesn't know about _textContent
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return this._textContent
       },
-      set textContent(value) {
+      set textContent(value: string) {
         // @ts-expect-error - TS doesn't know about _textContent
         this._textContent = value
         // @ts-expect-error - TS doesn't know about textChanges
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
         this.textChanges.push(value)
       },
       textChanges: []
@@ -76,7 +79,8 @@ describe('NotesPopup', () => {
     const storyNotesInput = <HTMLInputElement>document.getElementById('storyNotes')
     storyNotesInput.value = 'Test note'
     await popup.save()
-    expect(global.chrome.storage.sync.set).toHaveBeenCalledWith({'notes_123': 'Test note'})
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(chrome.storage.sync.set).toHaveBeenCalledWith({ notes_123: 'Test note' })
     // @ts-expect-error - Migrating from JS
     expect(popup.saveButton.textChanges).toContain('Saved!')
     // @ts-expect-error - Migrating from JS
@@ -86,7 +90,8 @@ describe('NotesPopup', () => {
   it('retrieves and displays saved note', async () => {
     mockedStory.id.mockResolvedValue('123')
     // @ts-expect-error - Migrating from JS
-    global.chrome.storage.sync.get.mockResolvedValue({'notes_123': 'Saved note'})
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    chrome.storage.sync.get.mockResolvedValue({ notes_123: 'Saved note' })
     const popup = new NotesPopup()
     await popup.set()
     // @ts-expect-error - Migrating from JS
@@ -110,10 +115,4 @@ describe('NotesPopup', () => {
     await expect(popup.set()).rejects.toThrow('storyNotesInput not found')
     expect(console.error).toHaveBeenCalled()
   })
-
-  it('handles message to check notes', async () => {
-    const fakeMessage = {message: 'checkNotes'}
-    await expect(NotesPopup.handleMessage(fakeMessage)).resolves.toBeUndefined()
-  })
-
 })
