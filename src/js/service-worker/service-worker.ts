eee36e8454836d75ba9208bad6e9676db4f42a51
Jens Astrup <jensaiden@gmail.com>
2024-06-09 20:55:15 -0400

    Remove analytics for todo initialization, improve updates by removing duplicate call to Todoist

--- src/js/service-worker/service-worker.ts
+++ src/js/service-worker/service-worker.ts
@@ -1,8 +1,5 @@
-import { sendEvent } from '@sx/analytics/event'
 import { handleCommands } from '@sx/service-worker/handlers'
 import checkHost from '@sx/utils/check-host'
-import { getSyncedSetting } from '@sx/utils/get-synced-setting'
-import scope from '@sx/utils/sentry'
 import { Story } from '@sx/utils/story'
 import Workspace from '@sx/workspace/workspace'
 
@@ -10,12 +7,10 @@ import { onInstallAndUpdate } from './on-install-and-update'
 import { SlugManager } from './slug-manager'
 
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 chrome.commands.onCommand.addListener(handleCommands)
 
 chrome.runtime.onInstalled.addListener(onInstallAndUpdate)
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo) {
   if (changeInfo.url && checkHost(changeInfo.url) && changeInfo.url.includes('story')) {
     Workspace.activate()
@@ -25,17 +20,6 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo) {
       message: 'update',
       url: changeInfo.url
     })
-    const enableTodoistOptions = await getSyncedSetting('enableTodoistOptions', false)
-    if (enableTodoistOptions) {
-      chrome.tabs.sendMessage(tabId, {
-        message: 'initTodos',
-        url: changeInfo.url
-      })
-      sendEvent('init_todos').catch((e) => {
-        console.error('Error sending event:', e)
-        scope.captureException(e)
-      })
-    }
     chrome.tabs.sendMessage(tabId, {
       message: 'initNotes',
       data: await Story.notes(),
