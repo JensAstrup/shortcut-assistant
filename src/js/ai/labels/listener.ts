01af44dea06ed472ceb3b18675537e3958cbd6ea
Jens Astrup <jensaiden@gmail.com>
2024-07-11 08:10:46 -0400

    Remove lint

--- src/js/ai/labels/listener.ts
+++ src/js/ai/labels/listener.ts
@@ -1,6 +1,5 @@
 import LabelsServiceWorker from '@sx/ai/labels/service-worker'
 import { AiProcessMessage } from '@sx/analyze/types/AiProcessMessage'
-import IpcRequest from '@sx/types/ipc-request'
 
 
 chrome.runtime.onMessage.addListener((request: AiProcessMessage, sender: chrome.runtime.MessageSender, sendResponse: (response: unknown) => void) => {
